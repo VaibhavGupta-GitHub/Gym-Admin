cadeb7c199c5ccfe756486e16185f9f262e300aa
saddam848811 <sh848811@gmail.com>
2025-07-19 13:40:32 +0530

    second commit

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\vaibh\OneDrive\Desktop\Projects\Gym Admin\backend\venv
+set VIRTUAL_ENV=C:\Users\sh848\Desktop\React+fastAPi\GYM Management\Gym-Admin\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
